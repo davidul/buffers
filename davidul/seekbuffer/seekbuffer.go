57ec5b638c5ee3fafb8685d1fed98d2e9d133776
David Ulicny <ulicny.david@gmail.com>
2023-10-28 08:15:39 +0200

    doc updates

--- davidul/seekbuffer/seekbuffer.go
+++ davidul/seekbuffer/seekbuffer.go
@@ -16,6 +16,7 @@ func NewEmptySeekBuffer() *SeekBuffer {
 	}
 }
 
+// buffer with initial content
 func NewSeekBuffer(src []byte) *SeekBuffer {
 	a := make([]byte, len(src))
 	copy(a, src)
@@ -25,42 +26,44 @@ func NewSeekBuffer(src []byte) *SeekBuffer {
 	}
 }
 
+// returns current content of the buffer
 func (s *SeekBuffer) Bytes() []byte {
 	return s.buffer
 }
 
+// appends content to the buffer
 func (s *SeekBuffer) Append(src []byte) {
 	s.buffer = append(s.buffer, src...)
 }
 
-func (s *SeekBuffer) Write(src []byte) {
+// writes content to the buffer, alias for Append
+func (s *SeekBuffer) Write(src []byte) (int, error) {
 	s.buffer = append(s.buffer, src...)
+	return len(src), nil
 }
 
+// reads content from the buffer into dst
 func (s *SeekBuffer) Read(dst []byte) (int, error) {
-
 	if s.offset >= len(s.buffer) {
 		return 0, io.EOF
 	}
 
 	n := copy(dst, s.buffer[s.offset:])
-
-	//var i = 0
-	//for ; i < len(dst) && i+s.offset < len(s.buffer); i++ {
-	//	dst[i] = s.buffer[i+s.offset]
-	//}
-	s.offset += n //len(dst)
+	s.offset += n
 	return n, nil
 }
 
+// rewinds the buffer to the beginning
 func (s *SeekBuffer) Rewind() {
 	s.offset = 0
 }
 
+// seeks to the offset
 func (s *SeekBuffer) Seek(offset int) {
 	s.offset = offset
 }
 
+// closes the buffer
 func (s *SeekBuffer) Close() error {
 	s.offset = 0
 	s.buffer = nil
